46fe4ce1180e464652b6557b92cdde239b9f0c23
vani <srivani.vaka@thundersoft.com>
2021-11-16 20:21:06 +0530

    arithematic opeartors using different data types.

---
+++ operators.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+float i=20.16, j=6.12;
+printf("The Addition of %f and %f is: %f\n",i,j,i+j);
+printf("The Subtraction of %f and %f is: %f\n",i,j,i-j);
+printf("The Multiplication of %f and %f is: %f\n",i,j,i*j);
+printf("The Division of %f and %f is: %f\n",i,j,i/j);
+printf("The Incremented value ++i is: %f\n",++i);
+printf("The Decremented value --j is: %f\n",--j);
+int a=30,b=25;
+printf("The Modulus operation of %d and %d is: %d\n",a,b,a%b);
+return 0;
+}
+
